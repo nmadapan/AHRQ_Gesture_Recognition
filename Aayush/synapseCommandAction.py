51816009a9729ce129fb7ba2ed70603d6f2aae2f
Aayush Dubey <aayush.dubey50@gmail.com>
2018-07-26 12:44:58 -0400

    Updated commands to read only integers

--- Aayush/synapseCommandAction.py
+++ Aayush/synapseCommandAction.py
@@ -8,7 +8,7 @@ def moveToImage(image):
 	newLocationX, newLocationY = auto.center(auto.locateOnScreen(image))
 	auto.moveTo(newLocationX, newLocationY)
 
-status = {prev_action: "", number_of_panels: 1}
+status = {"prev_action": "", "number_of_panels": 1}
 
 def get_status():
 	print "Status\n------"
@@ -45,108 +45,108 @@ while (True):
 	else:
 		print "Invalid command entered!\n"
 		continue
-	if (commandType == "0"):
-		if (commandID == "0"):
+	if (commandType == 0):
+		if (commandID == 0):
 			break
-		elif (commandID == "1"):
+		elif (commandID == 1):
 			get_status()
-	elif (commandType == "1"):
+	elif (commandType == 1):
 		scrollAmount = 10
-		if (commandID == "1"):
+		if (commandID == 1):
 			auto.scroll(scrollAmount)
-		elif (commandID == "2"):
+		elif (commandID == 2):
 			auto.scroll(-1 * scrollAmount)
 		else:
 			auto.click()
-	elif (commandType == "2" or commandType == "3"):
+	elif (commandType == 2 or commandType == 3):
 		if (commandType != "0"):
 			auto.click(button='right')
 			moveToImage('Images/scale-rotate-flip.png')
 			moveToImage('Images/flip-horizontal.png')
-			if (commandType == "2" and commandID == "2"):
+			if (commandType == 2 and commandID == 2):
 				moveToImage('Images/flip-vertical.png')
-			elif (commandType == "3" and commandID == "3"):
+			elif (commandType == 3 and commandID == 3):
 				moveToImage('Images/rotate-clockwise.png')
-			elif (commandType == "3" and commandID == "4"):
+			elif (commandType == 3 and commandID == 4):
 				moveToImage('Images/rotate-anticlockwise.png')
 		auto.click()
-	elif (commandType == "4"):
+	elif (commandType == 4):
 		scrollAmount = 10
-		if (commandID == "0"):
+		if (commandID == 0):
 			oldLocationX, oldLocationY = auto.position()
 			auto.click(button='right')
 			moveToImage('Images/zoom.png')
 			auto.click()
 			auto.moveTo(oldLocationX, oldLocationY)
-		elif (commandID == "1"):
+		elif (commandID == 1):
 			auto.moveDown()
 			auto.scroll(scrollAmount)
-		elif (commandID == "2"):
+		elif (commandID == 2):
 			auto.moveDown()
 			auto.scroll(-1 * scrollAmount)
-	elif (commandType == "5"):
+	elif (commandType == 5):
 		auto.click(button='right')
 		auto.click()
-	elif (commandType == "6"):
+	elif (commandType == 6):
 		oldLocationX, oldLocationY = auto.position()
 		auto.click(button='right')
 		moveToImage('Images/pan.png')
 		auto.click()
 		auto.moveTo(oldLocationX, oldLocationY)
 		dragAmount = 100
-		if (commandID == "1"):
+		if (commandID == 1):
 			auto.dragTo(dragAmount, 0, button='left')
-		elif (commandID == "2"):
+		elif (commandID == 2):
 			auto.dragTo(-1 * dragAmount, 0, button='left')
-		elif (commandID == "3"):
+		elif (commandID == 3):
 			auto.dragTo(0, dragAmount, button='left')
-		elif (commandID == "4"):
+		elif (commandID == 4):
 			auto.dragTo(0, -1 * dragAmount, button='left')
-	elif (commandType == "7"):
+	elif (commandType == 7):
 		oldLocationX, oldLocationY = auto.position()
 		auto.click(button='right')
 		moveToImage('Images/ruler.png')
 		auto.click()
 		auto.moveTo(oldLocationX, oldLocationY)
 		newLocationX, newLocationY = oldLocationX, oldLocationY
-		if (commandID == "1"):
+		if (commandID == 1):
 			auto.dragTo(newLocationX, newLocationY, button='left')
-		elif (commandID == "2"):
+		elif (commandID == 2):
 			auto.click(button='right')
 			moveToImage('Images/ruler-delete.png')
 			auto.click()
-	elif (commandType == "8"):
-		if (commandID == "1"):
+	elif (commandType == 8):
+		if (commandID == 1):
 			moveToImage('Images/series-thumbnail.png')
 			auto.click()
-		elif (commandID == "2"):
+		elif (commandID == 2):
 			moveToImage('Images/series-thumbnail-close.png')
 			auto.click()
-	elif (commandType == "9"):
-		if (commandID == "1"):
+	elif (commandType == 9):
+		if (commandID == 1):
 			moveToImage('Images/series-thumbnail.png')
 			auto.click()
-		elif (commandID == "2"):
+		elif (commandID == 2):
 			moveToImage('Images/series-thumbnail-close.png')
 			auto.click()
-		elif (commandID == "3"):
+		elif (commandID == 3):
 			moveToImage('Images/series-thumbnail-close.png')
 			auto.click()
-	elif (commandType == "10"):
-		if (commandID == "1"):
+	elif (commandType == 10):
+		if (commandID == 1):
 			moveToImage('Images/series-thumbnail.png')
 			auto.click()
-		elif (commandID == "2"):
+		elif (commandID == 2):
 			moveToImage('Images/series-thumbnail-close.png')
 			auto.click()
-	elif (commandType == "11"):
-		if (commandID == "1"):
+	elif (commandType == 11):
+		if (commandID == 1):
 			moveToImage('Images/series-thumbnail.png')
 			auto.click()
-		elif (commandID == "2"):
+		elif (commandID == 2):
 			moveToImage('Images/series-thumbnail-close.png')
 			auto.click()
 	auto.moveTo(self.width / 2, self.height / 2)
-	status["prev_action"] = commandList[int(commandType)][int(commandID)]
+	status["prev_action"] = commandList[commandType][commandID]
 
 
