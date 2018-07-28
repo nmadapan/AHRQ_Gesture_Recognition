52abd74099e06ab9b1f94596715afb24adb73853
Naveen Madapana <naveen.madapana@gmail.com>
2018-07-28 18:59:50 -0400

    verify lexicons is done!

--- Naveen/verify_lexicons.py
+++ Naveen/verify_lexicons.py
@@ -16,6 +16,7 @@ from copy import deepcopy
 ## Global Variables
 lex_folder = '..\\Data\\L3' # Where to write the files
 fps = 30
+default_width, default_height = 1920, 1080
 
 ## Initialization
 cmd_dict = json_to_dict('commands.json')
@@ -39,10 +40,12 @@ else: M = 3
 if(expect_num_inst%2 == 1):	N = 1 + expect_num_inst/M
 else: N = expect_num_inst/M
 
+des_w, des_h = default_width/(N+1), default_height/(M+1)
+
 for _ in range(M):
 	temp = []
 	for _ in range(N):
-		temp.append(np.zeros((1080/(M+1), 1920/(N+1), 3)))
+		temp.append(255 * np.ones((des_h, des_w, 3)))
 	bframe.append(temp)
 
 # for key, value in class_dict.items():
@@ -62,13 +65,13 @@ while(True):
 			j = idx/M
 			i = idx - M * j
 			ret, frame = vcap.read()
-			if ret: frame = cv2.resize(frame, None, fx = 1.0/(N+1), fy = 1.0/(M+1))
-			else: frame = np.zeros((1080/(M+1), 1920/(N+1), 3))
+			if ret: frame = cv2.resize(frame, dsize=(des_w, des_h)) 
+			else: frame = 255*np.ones((des_h, des_w, 3))
 			bframe[i][j] = np.uint8(frame)
 		cframe = []
 		for sublist in bframe: cframe.append(np.concatenate(sublist, axis = 1))
 		cframe = np.concatenate(cframe, axis = 0)
-		cv2.putText(cframe,cmd_dict[cmd], (1920/(N+2), 100), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (120,50,220),3,cv2.LINE_AA)
+		cv2.putText(cframe,cmd_dict[cmd], (default_width/(N+2), 100), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (120,50,220),3,cv2.LINE_AA)
 		cv2.imshow('Full Frame', np.uint8(cframe))
 
 		key = cv2.waitKey(1000/fps)
@@ -79,6 +82,3 @@ while(True):
 	if(cmd_idx>=len(cmds)): cmd_idx = len(cmds) - 1
 
 	for vcap in vcaps: vcap.release()
-
-
-
