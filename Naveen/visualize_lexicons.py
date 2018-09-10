eb7f208019c69d5f3d54734f81246704d3a28ca8
Naveen Madapana <naveen.madapana@gmail.com>
2018-09-10 16:39:51 -0400

    visualize lexicons hot fix

--- Naveen/visualize_lexicons.py
+++ Naveen/visualize_lexicons.py
@@ -14,7 +14,7 @@ from copy import deepcopy
 ###
 
 ## Global Variables
-lex_folder = 'D:\\AHRQ\\Study_IV\\Data\\Data\\L3' # Where to write the files
+lex_folder = 'H:\AHRQ\Study_IV\Data\Data\L2' # Where to write the files
 fps = 120
 default_width, default_height = 1920, 1080
 
@@ -56,6 +56,12 @@ close_flag = False
 cmd_idx = 0
 while(True):
 	cmd = cmds[cmd_idx]
+
+	## Resetting the bframe
+	for idx1 in range(len(bframe)):
+		for idx2 in range(len(bframe[0])):
+			bframe[idx1][idx2][:] = 255 * np.ones((des_h, des_w, 3))
+
 	if(close_flag): break
 
 	vids = glob(join(lex_folder, cmd+'*_rgb.avi'))
