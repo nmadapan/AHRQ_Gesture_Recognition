53a9d071f5580f2687a408745c85987e46a2b7a1
Naveen Madapana <nmadapan>
2018-10-17 16:52:16 -0400

    Updated file with more commands to test with

--- Naveen/Backup/temp_synapse_call.py
+++ Naveen/Backup/temp_synapse_call.py
@@ -10,8 +10,12 @@ import traceback
 # 
 # Happens bacuse of the socket.setdefaulttimeout(2) in ex_client. If 2 is made 10 then it's working fine. Checked many times
 ####
-#"1_1 40", "1_2 30",
-dataList = ["2_1", "2_2", "3_1", "3_2", "4_1 250_600_450", "4_2 30", "5_1", "5_2", "5_3", "5_4", "6_1", "6_2 500_630", "6_3 80", "6_4 40", "7_1 500_600_700_800", "7_1 400_300_900_500", "7_2 2", "7_2 1" "8_1", "8_2", "9_1 60", "9_2 30", "11_1", "11_2"]
+#"1_1 40", "1_2 30","5_1", "5_2"
+#dataList = ["0_8", "0_3", "2_1", "2_2", "3_1", "3_2", "4_1 250_600_450", "4_2 30", "5_3", "5_4", "0_2", "6_1", "6_2 500_630", "6_3 80", "6_4 40", "7_1 500_600_700_800", "7_1 400_300_900_500", "7_2 2", "7_2 1", "8_1", "8_2", "9_1 60", "9_2 30", "11_1", "11_2"]
+
+# 0_3 to toggle between keyboard and images...the first 0_3 switches from keyboard to images
+#"4_1 400", "4_2 30", "6_1", "6_2 900_630", "6_3 80", "6_4 40", "9_1 60", "9_2 30", "11_1", "11_2"
+dataList = ["0_3", "0_8", "2_1", "2_2", "3_1", "3_2"]
 
 ## Global Static Variables
 # Call Aayush's Synapse command script
@@ -24,6 +28,7 @@ while True and i < len(dataList):
     #    wait_for_connection expects to receive INITIAL_MESSAGE
     try:
         data = dataList[i]
+        print "Running command: " + str(data)
         # print data
         ### Temporary
         # Find a way to monitor synapse. when we should we send False ?
@@ -34,7 +39,9 @@ while True and i < len(dataList):
 
         if synapse_Flag:
             print data,'has been executed'
-            #self.client.send(str(True)) # Send True once the execuction is done.
+            #self.client.send(str(True)) # Send True once the execution is done.
+        else:
+            print data,'has failed execution'
         i += 1
         
     except Exception as exp:
