minor changes to realtime_main related to only skeleton is global now, some fixes inside the not only skeleton block. Some changes in cpm code related to normalization

Naveen/run_svm.py
@@ -8,31 +8,32 @@ from helpers import skelfile_cmp
 import matplotlib.pyplot as plt
 plt.rcdefaults()
 
+num_fingers = 0
 skel_folder_path = r'H:\AHRQ\Study_IV\Flipped_Data\L3'
 
 annot_folder_path = os.path.join(skel_folder_path, 'Annotations')
 dirname = os.path.dirname(skel_folder_path)
 fileprefix = os.path.basename(skel_folder_path)
 
-out_pkl_fname = os.path.join(dirname, fileprefix+'_data.pickle')
+out_pkl_fname = os.path.join(dirname, fileprefix+'_'+str(num_fingers)+'_data.pickle')
 
 fe = FeatureExtractor(json_param_path = 'param.json')
 out = fe.generate_io(skel_folder_path, annot_folder_path)
 
 # # ## Appending finger lengths
 
-# pickle_path1 = r'H:\AHRQ\Study_IV\Data\Data_cpm\fingers\L3'
-# with open(os.path.join(pickle_path1, 'L3_fingers_recent.pkl'), 'rb') as fp:
-# 	fingers_data = pickle.load(fp)
-# data_merge=[]
-# for txt_file in fe.skel_file_order:
-# 	key = os.path.splitext(txt_file)[0].split('_')[:-1]
-# 	s='_'
-# 	key=s.join(key)
-# 	for line in np.round(fingers_data.get(key),4):
-# 		data_merge.append(line)
+pickle_path1 = r'H:\AHRQ\Study_IV\Data\Data_cpm\fingers\L3'
+with open(os.path.join(pickle_path1, 'L3_fingers_norm_30033.pkl'), 'rb') as fp:
+	fingers_data = pickle.load(fp)
+data_merge=[]
+for txt_file in fe.skel_file_order:
+	key = os.path.splitext(txt_file)[0].split('_')[:-1]
+	s='_'
+	key=s.join(key)
+	for line in np.round(fingers_data.get(key),4):
+		data_merge.append(line)
 
-# out['data_input'] = np.concatenate([out['data_input'], np.array(data_merge)], axis = 1)
+out['data_input'] = np.concatenate([out['data_input'][:np.array(data_merge).shape[0]], np.array(data_merge)], axis = 1)
 
 # Randomize data input and output
 temp = zip(out['data_input'], out['data_output'])
@@ -60,6 +61,5 @@ plt.grid(True)
 #plt.show()
 
 clf, _, _ = fe.run_svm(out['data_input'], out['data_output'], train_per = 0.60)
-
 with open(out_pkl_fname, 'wb') as fp:
 	pickle.dump({'fe': fe, 'out': out}, fp)