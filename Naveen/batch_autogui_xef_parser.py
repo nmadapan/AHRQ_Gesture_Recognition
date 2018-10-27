6e54900651d1c4be9d2a26f07bbe0d5ccf398945
Naveen Madapana <nmadapan>
2018-10-27 12:32:41 -0400

    Updates

--- Naveen/batch_autogui_xef_parser.py
+++ Naveen/batch_autogui_xef_parser.py
@@ -24,18 +24,18 @@ from helpers import *
 #####################
 
 # Initialization
-xef_folder_paths = [r'E:\AHRQ\XEF_Files\S6_L3']
+xef_folder_paths = [r'D:\AHRQ\Study_IV\XEF_Files\*']
 in_format_flag = True # True since the filename is in the correct format
 enable_repeat = True # If True, all xef files will be executed, Otherwise, only the files that werent read previously or the files that were incompletely read
 xef_rgb_factor = 3.2 # Max is 3.8
 print_fnames = False
-images_folder = '.\\Images_Rahul'
+images_folder = '.\\Images_AHRQ'
 
-error_log_folder = '.\\Logfiles'
+error_log_folder = r'D:\AHRQ\Study_IV\XEF_Files\Logfiles'
 error_log_filename = os.path.join(error_log_folder, 'error_log_'+datetime.now().strftime("%Y_%m_%d_%H_%M")+'.txt')
 
 # Default settings
-base_write_folder = 'D:\\AHRQ\\Study_IV\\Data\\Data' # Where to write the files
+base_write_folder = r'D:\AHRQ\Study_IV\XEF_Files\Data' # Where to write the files
 kinect_studio_open_time = 3 # in seconds
 thresh_empty_cycles = 200 # No. of cycles to wait before obtaining the first RGB image. Quit after 200 cycles. 
 dynamic_thresh_fac = 10 # How long to wait later for the arrival of RGB frame 
