dfc01fa470b9b51aca9dbc8c50cc0194894da319
gonza337 <gonza337@gonza337s-MacBookPro141-13-inch-2017-Two-TB3-ports.local>
2018-11-05 19:04:17 -0500

    Synapse calibration working, next gesture working with regular test

--- Naveen/Synapse/SynapseAction.py
+++ Naveen/Synapse/SynapseAction.py
@@ -48,9 +48,29 @@ class SynapseAction:
         self.rightBoxH = None
 
         # Status of the system:
-        self.status = {}
+        self.status = {"prev_action": "", "panel_dim": [1, 1],
+                "window_open": False, "active_panel": [1, 1],
+                "rulers": {"len": 0}, "toUse": "Keyboard",
+                "hold_action": None, "defaultCommand": None, "group1_command": None,
+                "firstW": None, "firstH": None, "jumpW": None,"jumpH": None}
 
-    # Closes/Minimizes every window and leaves active the windows
+        # Command action list. The first row is an internal action list for self management
+
+        self.actionList = [["Admin", "Quit", "Get Status", "Switch ToUse", "Reset"],
+                ["Scroll", "Up", "Down"],
+                ["Flip", "Horizontal", "Vertical"],
+                ["Rotate", "Clockwise", "Counter-Clockwise"],
+                ["Zoom", "In", "Out"],
+                ["Switch Panel", "Left", "Right", "Up", "Down"],
+                ["Pan", "Left", "Right", "Up", "Down"],
+                ["Ruler", "Measure", "Delete"],
+                ["Window", "Open", "Close"],
+                ["Manual Contrast", "Increase", "Decrease"],
+                ["Layout", "One-Panel", "Two-Panels", "Three-Panels", "Four-Panels"],
+                ["Contrast Presets", "I", "II"]]
+
+
+# Closes/Minimizes every window and leaves active the windows
     # with the name "toOpen"
     def openWindow(self, toOpen):
             # Look into "start" command for Windows
@@ -77,9 +97,7 @@ class SynapseAction:
 
     # Prompt notification if choosing to remove the images
     def removeImagesPrompt(self):
-            self.openWindow(self.prompt)
-            # TODO uncomment this line
-            # (self.status["hold_action"], self.status["defaultCommand"], self.status["group1_command"]) = (None, None, None)
+            (self.status["hold_action"], self.status["defaultCommand"], self.status["group1_command"]) = (None, None, None)
             f = open(self.calibrationPath, "w")
             f.write(json.dumps(self.status, indent=4, separators=(',', ': ')))
             f.close()
@@ -91,21 +109,187 @@ class SynapseAction:
                     else: print "Unrecognized request, please enter either 'y' or 'n'"
             print ""
 
+    def get_bbox(self, before, after, thresholds = None, draw = False):
+            if (type(before) != type(after)):
+                    return ValueError('before and after should have same type')
+            if (isinstance(before, str)):
+                    if (not os.path.isfile(before)):
+                            sys.exit(before + ' does NOT exist')
+                    if (not os.path.isfile(after)):
+                            sys.exit(after + ' does NOT exist')
+                    before = cv2.imread(before)
+                    after = cv2.imread(after)
+            elif (isinstance(before, np.ndarray)):
+                    assert before.ndim == 3 and after.ndim == 3, 'before and after needs to be rgb arrays'
+                    assert (before.shape[0] == after.shape[0]) and (before.shape[1] == after.shape[1]),\
+                             'before and after arrays should be of same dimension'
+
+            dif = cv2.cvtColor(np.uint8(np.abs(after - before)), cv2.COLOR_BGR2GRAY)
+            _, dif = cv2.threshold(dif,127,255,0)
+
+            x_sum = np.mean(dif, axis = 0)
+            y_sum = np.mean(dif, axis = 1)
+
+            if (thresholds == None):
+                    x_thresh = np.mean(x_sum)
+                    #y_thresh = x_thresh
+                    y_thresh = np.mean(y_sum)
+            else:
+                    x_thresh = thresholds[0]
+                    y_thresh = thresholds[1]
+
+            x_sum, y_sum = x_sum > x_thresh, y_sum > y_thresh
+
+            x1, x2 = np.argmax(x_sum), x_sum.size - np.argmax(np.flip(x_sum, 0)) - 1
+            y1, y2 = np.argmax(y_sum), y_sum.size - np.argmax(np.flip(y_sum, 0)) - 1
+
+            if (draw):
+                    after = cv2.rectangle(after,(x1,y1),(x2,y2),(0,255,0),4)
+                    cv2.imshow('Frame', cv2.resize(after, None, fx=0.5, fy=0.5))
+                    cv2.waitKey(0)
+
+            return (x1, y1, x2, y2)
+
+
+    def resetPanelMoves(self):
+            self.status["firstW"] = (float(self.width) / (float(self.status["panel_dim"][1]) * 2.0))
+            self.status["firstH"] = (float(self.height) / (float(self.status["panel_dim"][0]) * 2.0))
+            self.status["jumpW"] = (self.status["firstW"] * 2.0 if self.status["panel_dim"][1] != 1 else 0)
+            self.status["jumpH"] = (self.status["firstH"] * 2.0 if self.status["panel_dim"][0] != 1 else 0)
+
+    def moveToActivePanel(self):
+            moveToX = self.status["firstW"] + (self.status["active_panel"][1] - 1) * (self.status["jumpW"])
+            moveToY = self.status["firstH"] + (self.status["active_panel"][0] - 1) * (self.status["jumpH"])
+            auto.moveTo(moveToX, moveToY)
+
+    # Reset height of top bar and save it
+    def resetTopBarHeight(self):
+            self.moveToActivePanel()
+            auto.click()
+            ImageGrab.grab(bbox=(0, self.macH, self.nativeW, self.nativeH)).save(os.path.join("SCA_Images", "fullscreen.png"))
+            auto.moveTo(auto.position()[0], 0)
+            time.sleep(1)
+            ImageGrab.grab(bbox=(0, self.macH, self.nativeW, self.nativeH)).save(os.path.join("SCA_Images", "afterTopBar.png"))
+            topBarBox = self.get_bbox(os.path.join("SCA_Images", "fullscreen.png"), os.path.join("SCA_Images", "afterTopBar.png"))
+            self.topBarHeight = topBarBox[3] - topBarBox[1] + 1
+            ImageGrab.grab(bbox=(0, self.macH, self.nativeW, self.topBarHeight + self.macH)).save(os.path.join("SCA_Images", "topBar.png"))
+
+    # Reset the right click
+    def resetRightClick(self):
+            self.moveToActivePanel()
+            auto.click()
+            beforeRightPath = os.path.join("SCA_Images", "RightClick", "beforeRight.png")
+            ImageGrab.grab(bbox=self.boundBoxNoDash).save(beforeRightPath)
+            auto.click(button='right')
+            time.sleep(1)
+            afterRightPath = os.path.join("SCA_Images", "RightClick", "afterRight.png")
+            ImageGrab.grab(bbox=self.boundBoxNoDash).save(afterRightPath)
+            rightBox = self.get_bbox(beforeRightPath, afterRightPath)
+            print "rightBox: %s" % (rightBox,)
+            (self.rightBoxW, self.rightBoxH) = (rightBox[2] - rightBox[0] + 1, rightBox[3] - rightBox[1] + 1)
+            print "rightBox WxH: %s" % ((self.rightBoxW, self.rightBoxH),)
+            self.optionH = (self.rightBoxH * 36.0 / 1000.0) / self.scale
+            self.rightHR = (self.rightBoxH * 10.0 / 1000.0) / self.scale
+            self.rightPlus = (self.rightBoxH * 8.0 / 1000.0) / self.scale
+            self.rightIcons = (self.rightBoxH * 50.0 / 1000.0)
+            self.rightOffset = (self.rightBoxH * 58.0 / 1000.0)
+
+            (rightx1, righty1) = (rightBox[0] + self.boundBoxNoDash[0], rightBox[1] + self.boundBoxNoDash[1])
+            (x1, y1) = (rightx1 + self.rightIcons, righty1 + self.rightOffset)
+            (x2, y2) = (rightx1 + self.rightBoxW, righty1 + self.rightBoxH)
+            ImageGrab.grab(bbox=(x1, y1, x2, y2)).save(os.path.join("SCA_Images", "RightClick", "rightClick.png"))
+
+            self.moveToActivePanel()
+            auto.click()
+
+    # Reset rightClick's presets or its scaleRotateFlip
+    def resetRightOptions(self, option, offset):
+            self.moveToActivePanel()
+            auto.click()
+            auto.click(button='right')
+            located = auto.locateOnScreen(os.path.join("SCA_Images", "RightClick", "rightClick.png"))
+            if (located is not None): (rightx1, righty1, w, h) = located
+            else:
+                    print "Cannot find " + option + " on calibration reset. Attempting reset on rightClick."
+                    self.resetRightClick()
+                    located = auto.locateOnScreen(os.path.join("SCA_Images", "RightClick", "rightClick.png"))
+                    if (located is not None): (rightx1, righty1, w, h) = located
+                    else:
+                            print "Failed to reset " + option + "."
+                            return
+            moveToX = (rightx1 + w / 2.0) / self.scale
+            moveToY = (righty1 / self.scale) + (self.optionH * offset) + self.rightHR
+            auto.moveTo(moveToX, moveToY)
+            time.sleep(1)
+            self.moveToActivePanel()
+            auto.press("0")
+            afterRightPath = os.path.join("SCA_Images", "RightClick", "afterRight.png")
+            afterOptionPath = os.path.join("SCA_Images", "RightClick", "after" + option + ".png")
+            ImageGrab.grab(bbox=self.boundBoxNoDash).save(afterOptionPath)
+            box = self.get_bbox(afterRightPath, afterOptionPath)
+            print option + " box: %s" % (box,)
+            (boxW, boxH) = (box[2] - box[0] + 1, box[3] - box[1] + 1)
+            print option + " box WxH: %s" % ((boxW, boxH),)
+            (x1, y1) = (box[0], box[1])
+            optionPath = os.path.join("SCA_Images", "RightClick", option + ".png")
+            ImageGrab.grab(bbox=(x1 + self.rightIcons, y1, x1 + boxW, y1 + boxH)).save(optionPath)
+            auto.click()
+
+    def resetAll(self):
+            print "\nWarming up synapse system...\n"
+            self.resetTopBarHeight()
+            self.resetRightClick()
+            self.resetRightOptions("presets", 8.5)
+            self.resetRightOptions("scaleRotateFlip", 9.5)
+            print "\nCompleted warm-up, make your gestures!\n"
+
+
+    # Report situation to command prompt, useful for debugging and for users understanding an issue.
+    def promptNotify(self,message, sleepAmt):
+            openWindow(prompt)
+            print message
+            time.sleep(sleepAmt)
+
+    # Find Synapse's rightClick image
+    def findRightClick(self,offset):
+            located = auto.locateOnScreen(os.path.join("SCA_Images", "RightClick", "rightClick.png"))
+            if (located is not None):
+                    (x1, y1, w, h) = located
+                    auto.moveTo((x1 / self.scale) + (w / 2.0) * self.scale, (y1 + (offset / 1000.0) * self.status["rightBoxH"]) / self.scale)
+                    return True
+            else:
+                    print "Error when performing right click function."
+                    return False
+
     # Handles the killing signals of the class
     def signalHandler(self, sig, frame):
-	self.removeImagesPrompt()
-	sys.exit(0)
+        self.removeImagesPrompt()
+        sys.exit(0)
 
-    def start(self):
-        # set up the signal handler
-        signal.signal(signal.SIGINT, self.signalHandler)
+    def calibrate(self):
 
-        while True:
-            continue
+        # if there is a calibration file, use it, if not
+        # generate a calibration
+        self.resetPanelMoves()
+        self.openWindow(self.viewer)
+
+        if (os.path.exists(self.calibrationPath)):
+            f = open(self.calibrationPath, "r")
+            self.status = json.loads(" ".join([line.rstrip('\n') for line in f]))
+            f.close()
+        else:
+            self.resetAll()
+            f = open(self.calibrationPath, "w")
+            f.write(json.dumps(self.status, indent=4, separators=(',', ': ')))
+            f.close()
 
 if __name__ == "__main__":
     syn_action = SynapseAction()
+    # set up the signal handler
+    signal.signal(signal.SIGINT, syn_action.signalHandler)
     # Run the program
-    syn_action.start()
+    syn_action.calibrate()
+    while True: continue
+
 
 
--- Naveen/SynapseCommand.py
+++ Naveen/SynapseCommand.py
@@ -4,7 +4,7 @@
 #
 #####################
 import json
-from helpers import file_to_list
+from helpers import file_to_list, json_to_dict
 import os
 
 
@@ -23,19 +23,36 @@ class SynapseCommand():
 		# Gesture desambiguation lists
 		data_folder = os.path.join("..","Data")
 		# Context commands
-		self.context_list = file_to_list(os.join.path(data_folder),lexicon+"_context.txt")
+		self.context_list = file_to_list(os.path.join(data_folder,lexicon+"_context.txt"))
+                # Flatten the list
+		self.context_list = [item for sublist in self.context_list for item in sublist]
 		# Modifiers that are so similar that can be confused
-		self.similar_modifiers = file_to_list(os.join.path(data_folder),lexicon+"_similar_m.txt")
+		self.similar_modifiers = file_to_list(os.path.join(data_folder,lexicon+"_similar_m.txt"))
 		# Gestures that are so similar that can be confused
-		self.similar_gestures = file_to_list(os.join.path(data_folder),lexicon+"_similar_g.txt")
+		self.similar_gestures = file_to_list(os.path.join(data_folder,lexicon+"_similar_g.txt"))
 		# Repeated gestures (were not trained to be different in the ML)
-		self.repeated_gestures = file_to_list(os.join.path(data_folder),lexicon+"_reps.txt")
+		self.repeated_gestures = file_to_list(os.path.join(data_folder,lexicon+"_reps.txt"))
 
+        def get_lists_with_gesture(self,gesture,gesture_list):
+            similar_list_index = [gesture in mod_list for mod_list in gesture_list]
+            similar_list_index =  [i for i, v in enumerate(similar_list_index) if v]
+            return [gesture_list[index] for index in similar_list_index]
 
-	def get_similar_command(self,list):
-		pass
+	def get_repeated_modifiers(self,modifier):
+            repeated_with_modifier = self.get_lists_with_gesture(modifier,self.repeated_gestures)
 
+            def filter_non_modifiers(elem):
+                context_substrings = [e.split("_")[0]+"_" for e in self.context_list]
+                return not elem.split("_")[0]+"_" in context_substrings
 
+            repeated_modifiers = [filter(filter_non_modifiers,sub_list) for sub_list in repeated_with_modifier]
+            return repeated_modifiers
+
+        def context_action_err(self):
+            print("Inconsistent context + action")
+            self.inconsistent_context_action_err =+ 1
+            self.command = None
+            self.context = None
 
 	def get_command(self, rcv_command):
 		#####################
@@ -54,8 +71,14 @@ class SynapseCommand():
 
 			# then in the modifiers, check if you have a list of contexts, if you do,
 			# desambiguate according to the context.
+
 		# if a modifier with context is executed
-		elif rcv_context_num in [num.split("_")[0] for num in context_list]:
+		elif rcv_context_num in [num.split("_")[0] for num in self.context_list]:
+                        ############# Load some conditions #############
+                        modifier_in_repeated_commads = any([rcv_command in mod_list for mod_list in self.repeated_gestures])
+                        modifier_in_similar_comands = any([rcv_command in mod_list for mod_list in self.similar_modifiers])
+                        ################################################
+
 			# Check if the context was executed
 			if self.context is None:
 				print("You executed an action without it's context")
@@ -64,29 +87,38 @@ class SynapseCommand():
 				return None
 
 			# Check if the modifier is a modifier of that context
-			current_context_num = self.context.split("_")[0]
-			elif rcv_context_num == current_context_num:
-					self.command = rcv_command
+			elif rcv_context_num == self.context.split("_")[0]:
+                                self.command = rcv_command
 			# Check if the modifier is correct but re-used under another label
-
-
-			# Check if the modifier is one that it can get confused with.
-			elif any([rcv_command in mod_list for mod_list in self.similar_modifiers]):
-				# Get the list of possible similar modifiers
-				similar_list_index = [rcv_command in mod_list for mod_list in self.similar_modifiers]
-				similar_list_index =  next(i for i, v in enumerate(similar_list_index) if v)
-				similar_candidates = self.similar_modifiers[similar_list_index]
-				self.modifier_replaced += 1
-				# Get the modifier that has the matching context from the list
-				replaced_command = [cmd for cmd in similar_candidates if \
-									cmd.split("_")[0]==self.current_context_num][0]
+			elif modifier_in_repeated_commads or modifier_in_similar_comands:
+                            repeated_labels = self.get_repeated_modifiers(modifier, self.repeated_gestures)
+                            if len(repeated_labels[0] != 0):
+                                for rep_list in repeated_labels:
+                                    # Get the modifier that has the matching context from the list
+                                    replaced_command = [cmd for cmd in rep_list if \
+                                                        cmd.split("_")[0]==self.current_context_num][0]
+                                    if len(replaced_command) > 0:
+                                        replaced_command = replaced_command[0]
+                                        self.modifier_replaced += 1
+                                        self.command = replaced_command
+                                    # Check if the modifier is getting confused with a similar modifier
+                                    elif modifier_in_similar_comands:
+                                            # Get the list of possible similar modifiers
+                                            similar_candidates = self.get_lists_with_gesture(modifier,self.similar_modifiers)
+                                            # Get the modifier that has the matching context from the list
+                                            replaced_command = [cmd for cmd in similar_candidates if \
+                                                                cmd.split("_")[0]==self.current_context_num]
+                                            if len(replaced_command) > 0:
+                                                replaced_command = replaced_command[0]
+                                                self.modifier_replaced += 1
+                                                self.command = replaced_command
+                                            else:
+                                                self.context_action_err()
+                                                return None
 			# If the modifier is inconsistent with the context:
 			else:
-				print("Inconsistent context + action")
-				self.inconsistent_context_action_err =+ 1
-				self.command = None
-				self.context = None
-				return None
+                            self.context_action_err()
+                            return None
 
 		# A command without context was executed. Reset everything and
 		# send the command.
@@ -95,6 +127,8 @@ class SynapseCommand():
 		else:
 			self.command = rcv_command
 			self.context = None
+
+                self.prev_commands.append(self.command)
 		return self.command
 
 	def update_gesture(self, received_):
@@ -109,5 +143,60 @@ class SynapseCommand():
 			'modifier_replaced': self.modifier_replaced
 		}
 		file.write(json.dumps(file_dict))
+
+if __name__ == '__main__':
+    syn_command = SynapseCommand('L2','test')
+    ### TEST 1 ###
+    # No conflicts
+    comand_list = ["1_0", "1_1", "1_2", "2_0", "2_1", "2_2", "3_0", "3_1", "3_2", "4_0", "4_1", "4_2", "5_0", "5_1", "5_2", "5_3", "5_4", "6_0", "6_1", "6_2", "6_3", "6_4", "7_0", "7_1", "7_2", "8_0", "8_1", "8_2", "9_0", "9_1", "9_2", "10_0", "10_1", "10_2", "10_3", "10_4", "11_0", "11_1", "11_2"]
+    for comand in comand_list:
+        print syn_command.get_command(comand)
+
+    ### TEST 2 ###
+    # modifier does not match context but it can be replaced by a similar modifier
+
+    ### TEST 3 ###
+    # modifier does not match context but it can be replaced by a repeaded gesture
+
+    ### TEST 4 ###
+    # modifier and gesture dont match.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
