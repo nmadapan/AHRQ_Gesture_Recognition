6c4f66fc5c8d71c4331978a3f036bee4985f282a
nmadapan <naveen.madapana@gmail.com>
2018-11-29 18:50:27 -0500

    run_svm_new_loop modified to incorporate a list of eliminated commands. results for T2 and T3 are added!

--- Naveen/run_svm_new_loop.py
+++ Naveen/run_svm_new_loop.py
@@ -7,10 +7,11 @@ from copy import deepcopy
 from glob import glob
 from random import shuffle
 from FeatureExtractor import FeatureExtractor
-from helpers import skelfile_cmp
+from helpers import *
 from sklearn.metrics import confusion_matrix
 from scipy import stats
 from DST import DST
+import time
 import matplotlib.pyplot as plt
 plt.rcdefaults()
 plt.ioff()
@@ -24,7 +25,7 @@ display = False
 write_flag = False
 
 ## Skeleton
-skel_folder_path = r'H:\AHRQ\Study_IV\NewData\L2'
+skel_folder_path = r'G:\AHRQ\Study_IV\NewData\L8'
 
 ## Variables
 all_subject_ids = ['S1', 'S2', 'S3', 'S4', 'S5', 'S6']
@@ -34,7 +35,7 @@ out_result_filename_suffix = '_result.pickle'
 result = {}
 
 ## CPM
-pickle_base_path1 = r'H:\AHRQ\Study_IV\Data\Data_cpm_new\fingers'
+pickle_base_path1 = r'G:\AHRQ\Study_IV\Data\Data_cpm_new\fingers'
 pickle_file_suffix = '_fingers_from_hand_base_equate_dim_subsample.pkl'
 
 pickle_path1 = os.path.join(pickle_base_path1,os.path.basename(skel_folder_path))
@@ -53,16 +54,30 @@ fe = FeatureExtractor(json_param_path = 'param.json')
 print('Generating IO: ', end = '')
 out = fe.generate_io(skel_folder_path, annot_folder_path)
 print('DONE !!!')
-body_data_input = deepcopy(out['data_input'])
-hand_data_input = None
-combined_data_input = None
 
 ## Extract some variables from fe object
 num_points = fe.fixed_num_frames
 num_fingers = fe.num_fingers
+id_to_labels = fe.id_to_labels
 skel_file_order = deepcopy(fe.skel_file_order)
 dominant_types_order = deepcopy(fe.dominant_type)
 assert len(dominant_types_order) == len(skel_file_order), 'ERROR! MISMATCH'
+
+## Extract from out object
+body_data_input = deepcopy(out['data_input'])
+data_output = deepcopy(out['data_output'])
+hand_data_input = None
+combined_data_input = None
+
+############################
+### Ignore some comamnds ###
+############################
+## The command ids to ignore
+## For task 2: 2, 5_3, 5_4, 7, 8, 9
+# ignore_command_ids_list = ['2_0', '2_1', '2_2', '5_3', '5_4', '7_0', '7_1', '7_2', '8_0', '8_1', '8_2', '9_0', '9_1', '9_2']
+## For Task 3: 3, 5_3, 5_4, 7, 8, 11
+ignore_command_ids_list = ['3_0', '3_1', '3_2', '5_3', '5_4', '7_0', '7_1', '7_2', '8_0', '8_1', '8_2', '11_0', '11_1', '11_2']
+########################
 
 if(ENABLE_FINGERS):
 	# # ## Appending finger lengths with dominant_hand first
@@ -83,9 +98,9 @@ if(ENABLE_FINGERS):
 				for frame in gesture:
 					gesture_shuffle.append(frame[num_fingers:].tolist()+frame[:num_fingers].tolist())
 				data_merge.append(np.array(gesture_shuffle).flatten().tolist())
-				
+
 	hand_data_input = deepcopy(np.array(data_merge))
-	full_data_input = np.concatenate([body_data_input, hand_data_input], axis = 1)
+	combined_data_input = np.concatenate([body_data_input, hand_data_input], axis = 1)
 	print('DONE !!!')
 
 ## Plotting histogram - No. of instances per class
@@ -111,58 +126,81 @@ for eliminate_subject_id in all_subject_ids:
 	## Leave One Subject Out - Create IDs
 	subject_name_order = np.array([fname.split('_')[2] for fname in skel_file_order])
 	partial_train_subject_flags = (subject_name_order != eliminate_subject_id)
-	all_train_flags = []
+	subject_flags = []
 	for idx, dom_types_list in enumerate(dominant_types_order):
-		all_train_flags += [partial_train_subject_flags[idx]]*len(dom_types_list)
-	all_train_flags = np.array(all_train_flags)
+		subject_flags += [partial_train_subject_flags[idx]]*len(dom_types_list)
+	subject_flags = np.array(subject_flags)
 
 	## Body + Hands
 	print('\nBody + Hand ====> SVM')
-	data_input, data_output = deepcopy(full_data_input), deepcopy(out['data_output'])
-	train_data_input = data_input[all_train_flags, :]
-	train_data_output = data_output[all_train_flags, :]
-	test_data_input = data_input[np.logical_not(all_train_flags), :]
-	test_data_output = data_output[np.logical_not(all_train_flags), :]
+
+	st = time.time()
+	t_data_in, t_data_out = deepcopy(combined_data_input), deepcopy(data_output)
+	old_to_new, cmd_flags = remove_some_classes(t_data_out, id_to_labels, ignore_command_ids_list)
+	train_flags = np.logical_and(cmd_flags, subject_flags)
+	test_flags = np.logical_and(cmd_flags, np.logical_not(subject_flags))
+	## Leave one subject out
+	train_data_input = t_data_in[train_flags, :]
+	train_data_output = modify_output_indices(t_data_out, old_to_new, train_flags)
+	print('New no. of classes: ', train_data_output.shape[1])
+	## Left out subject is the testing data
+	test_data_input = t_data_in[test_flags, :]
+	test_data_output = modify_output_indices(t_data_out, old_to_new, test_flags)
+
 	res = fe.run_svm(train_data_input, train_data_output, test_data_input, test_data_output, train_per = 0.80, inst_var_name = 'svm_clf', display = display)
 	result[eliminate_subject_id]['body_plus_hand']['train_acc'] = res['train_acc']
 	result[eliminate_subject_id]['body_plus_hand']['valid_acc'] = res['valid_acc']
 	result[eliminate_subject_id]['body_plus_hand']['test_acc_top1'] = res['test_acc_top1']
 	result[eliminate_subject_id]['body_plus_hand']['test_acc_top3'] = res['test_acc_top3']
-	result[eliminate_subject_id]['body_plus_hand']['test_acc_top4'] = res['test_acc_top5']
+	result[eliminate_subject_id]['body_plus_hand']['test_acc_top5'] = res['test_acc_top5']
 	full_prob = fe.svm_clf.predict_proba(test_data_input)
 	full_pred_output = np.argmax(full_prob, axis = 1)
 	print('DONE !!! Storing variable in svm_clf')
 
 	## Only Body
 	print('\nBody ====> SVM')
-	data_input, data_output = deepcopy(body_data_input), deepcopy(out['data_output'])
-	train_data_input = data_input[all_train_flags, :]
-	train_data_output = data_output[all_train_flags, :]
-	test_data_input = data_input[np.logical_not(all_train_flags), :]
-	test_data_output = data_output[np.logical_not(all_train_flags), :]
+	st = time.time()
+	t_data_in, t_data_out = deepcopy(body_data_input), deepcopy(data_output)
+	old_to_new, cmd_flags = remove_some_classes(t_data_out, id_to_labels, ignore_command_ids_list)
+	train_flags = np.logical_and(cmd_flags, subject_flags)
+	test_flags = np.logical_and(cmd_flags, np.logical_not(subject_flags))
+	## Leave one subject out
+	train_data_input = t_data_in[train_flags, :]
+	train_data_output = modify_output_indices(t_data_out, old_to_new, train_flags)
+	## Left out subject is the testing data
+	test_data_input = t_data_in[test_flags, :]
+	test_data_output = modify_output_indices(t_data_out, old_to_new, test_flags)
+
 	res = fe.run_svm(train_data_input, train_data_output, test_data_input, test_data_output, train_per = 0.80, inst_var_name = 'svm_clf_body', display = display)
 	result[eliminate_subject_id]['body']['train_acc'] = res['train_acc']
 	result[eliminate_subject_id]['body']['valid_acc'] = res['valid_acc']
 	result[eliminate_subject_id]['body']['test_acc_top1'] = res['test_acc_top1']
 	result[eliminate_subject_id]['body']['test_acc_top3'] = res['test_acc_top3']
-	result[eliminate_subject_id]['body']['test_acc_top4'] = res['test_acc_top5']	
+	result[eliminate_subject_id]['body']['test_acc_top5'] = res['test_acc_top5']
 	body_prob = fe.svm_clf_body.predict_proba(test_data_input)
 	body_pred_output = np.argmax(body_prob, axis = 1)
 	print('DONE !!! Storing variable in svm_clf_body')
 
 	## Only Hand
 	print('\nHand ====> SVM')
-	data_input, data_output = deepcopy(hand_data_input), deepcopy(out['data_output'])
-	train_data_input = data_input[all_train_flags, :]
-	train_data_output = data_output[all_train_flags, :]
-	test_data_input = data_input[np.logical_not(all_train_flags), :]
-	test_data_output = data_output[np.logical_not(all_train_flags), :]
+	st = time.time()
+	t_data_in, t_data_out = deepcopy(hand_data_input), deepcopy(data_output)
+	old_to_new, cmd_flags = remove_some_classes(t_data_out, id_to_labels, ignore_command_ids_list)
+	train_flags = np.logical_and(cmd_flags, subject_flags)
+	test_flags = np.logical_and(cmd_flags, np.logical_not(subject_flags))
+	## Leave one subject out
+	train_data_input = t_data_in[train_flags, :]
+	train_data_output = modify_output_indices(t_data_out, old_to_new, train_flags)
+	## Left out subject is the testing data
+	test_data_input = t_data_in[test_flags, :]
+	test_data_output = modify_output_indices(t_data_out, old_to_new, test_flags)
+
 	res = fe.run_svm(train_data_input, train_data_output, test_data_input, test_data_output, train_per = 0.80, inst_var_name = 'svm_clf_hand', display = display)
 	result[eliminate_subject_id]['hand']['train_acc'] = res['train_acc']
 	result[eliminate_subject_id]['hand']['valid_acc'] = res['valid_acc']
 	result[eliminate_subject_id]['hand']['test_acc_top1'] = res['test_acc_top1']
 	result[eliminate_subject_id]['hand']['test_acc_top3'] = res['test_acc_top3']
-	result[eliminate_subject_id]['hand']['test_acc_top4'] = res['test_acc_top5']	
+	result[eliminate_subject_id]['hand']['test_acc_top5'] = res['test_acc_top5']
 	hand_prob = fe.svm_clf_hand.predict_proba(test_data_input)
 	hand_pred_output = np.argmax(hand_prob, axis = 1)
 	print('DONE !!! Storing variable in svm_clf_hand')
