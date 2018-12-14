42ee2613648d4c265bb74a9836c6cb01d3a88ff9
Naveen Madapana <nmadapan>
2018-12-13 21:14:07 -0500

    record data systematically file is done. Verified.

--- Naveen/record_systematically.py
+++ Naveen/record_systematically.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 from glob import glob
-from os.path import join, isfile, dirname, basename
+from os.path import join, isfile, dirname, basename, splitext
 from helpers import *
 from KinectReader import kinect_reader
 from copy import deepcopy
@@ -14,16 +14,16 @@ from threading import Thread
 ##########################
 parser = argparse.ArgumentParser()
 parser.add_argument("-l", "--lexicon_id",
-					default='L2',
+					default='L10',
 					help=("Lexicon ID. For example: L2"))
 parser.add_argument("-s", "--subject_id",
 					default='S3',
 					help=("Subject ID. For example: S2"))
 parser.add_argument("-p", "--data_path",
-					default=r'G:\AHRQ\Study_IV\NewData',
+					default=r'E:\AHRQ\Study_IV\NewData',
 					help=("Data path to the folders containing L2/6/8..."))
 parser.add_argument("-wp", "--write_path",
-					default=r'G:\AHRQ\Study_IV\SurgeonData',
+					default=r'E:\AHRQ\Study_IV\SurgeonData',
 					help=("Data path to the folders containing L2/6/8..."))
 
 ### Arguments
@@ -46,46 +46,41 @@ for cmd in all_cmds:
 	if(len(vids)==0): cmds.remove(cmd); continue
 
 cmd_names = cmds
-window_key = None
-
-def stream_rgb(kr):
-	cv2.namedWindow('Recording Data')
-	while True:
-		rgb_flag = kr.update_rgb()
-		if(rgb_flag):
-			frame = deepcopy(kr.color_image)
-			frame = cv2.resize(frame, None, fx = 0.5, fy = 0.5)
-			cv2.imshow('Recording Data', frame)
-		window_key = cv2.waitKey(1)
-		if(window_key == ord('e')):
-			cv2.destroyAllWindows()
+print('No. of commands: ', len(cmd_names))
 
 if __name__ == '__main__':
-	kr = kinect_reader()
-	wait_for_kinect(kr)
-
-	th_stream = Thread(name = 'stream_thread', target = stream_rgb, args = (kr, ))
-	th_stream.start()
-
 	cmd_idx = 0
-	curr_status = False
+	reject_counter = 0
 
 	while True:
+		if(cmd_idx >= len(cmd_names)): break
 		cmd = cmd_names[cmd_idx]
-		key = cv2.waitKey(1)
-		if(key in [32, 10]): curr_status = True
-		## If prev status is false and curr status is true
-		if(curr_status):
-			xef_file_name = '_'.join(cmd, subject_id, lexicon_id, cmd_dict[cmd]+.xef)
+		value = raw_input('Do you want to continue? (Y/N): ')
+		if(value in ['y', 'Y']):
+			print 'Recording: ', cmd_dict[cmd]
+			name = cmd_dict[cmd]
+			if(len(name.split(' ')) == 2):
+				name = '_'.join(name.split(' '))
+			elif(len(name.split(' ')) == 1):
+				name = name + '_X'
+			xef_file_name = '_'.join([cmd, subject_id, lexicon_id, name + '.xef'])
+			print xef_file_name
 			parser = Parser(xef_file_name, write_path, in_format_flag = True, display = True)
 			parser.parse()
-		curr_status = False
-
-		inp = raw_input('Accept(y)/Reject(n)')
-		if(inp in ['y', 'Y', 10]):
-			## Leave the files as they are
-			cmd_idx += 1
-			pass
+		
+			inp = raw_input('Accept(y)/Reject(n): ')
+			if(inp in ['y', 'Y']):
+				reject_counter = 0
+				cmd_idx += 1
+			else:
+				reject_counter += 1
+				## Rename the files
+				curr_files = glob(join(join(write_path, lexicon_id), splitext(xef_file_name)[0] + '*'))
+				curr_files = [cfile for cfile in curr_files if 'reject' not in cfile] 
+				for curr_file in curr_files:
+					old_path = curr_file
+					fname, ext = splitext(basename(curr_file))[0], splitext(basename(curr_file))[1]
+					new_path = join(join(write_path, lexicon_id), fname + '_reject' + str(reject_counter)+ ext)
+					os.rename(old_path, new_path)
 		else:
-			## Rename the files
-			pass
+			break
